b7e903a47659af07ee356a827029e804a03031f4
Damian Irarrazaval <damian_irarrazaval@hotmail.com>
2015-08-23 19:09:55 -0300

    Pony Sinatra Archives

---
+++ sinatra.rb
@@ -0,0 +1,35 @@
+require "sinatra"
+require "pony"
+
+get '/' do
+  send_file File.join(settings.public_folder, "index.html")
+end
+
+get "/contact" do
+
+Pony.mail({
+  :to => 'damianirarrazavalb@gmail.com',
+  :from => params[:from],
+  :body => params[:message],
+  :subject => "#{params[:name]} #{params[:phone]}",
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'damianirarrazavalb@gmail.com',
+    :password             => 'wzmrwnxdmcuestvf',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+})
+
+  file_blank = open("contact_list.txt", "w")
+  inputs = params[:name], params[:from], params[:phone], params[:message] 
+  file_blank.write(inputs)
+
+	redirect to("/")
+
+end
+
+
